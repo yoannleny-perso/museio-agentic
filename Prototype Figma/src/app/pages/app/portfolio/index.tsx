23ebe3d30fd5bc364c9dcb907bbe2975733cf907
yoann leny <yoann.leny@monks.com>
2026-04-23 00:18:21 -0400

    Remove Prototype Figma from repository

--- Prototype Figma/src/app/pages/app/portfolio/index.tsx
+++
@@ -1,6 +0,0 @@
-// V3 Portfolio System - Main export file
-export { PortfolioHome } from './PortfolioHome';
-export { PortfolioBuilder } from './PortfolioBuilder';
-export { ThemeStudio } from './ThemeStudio';
-export { PortfolioInsights } from './PortfolioInsights';
-export { BlockEditor } from './BlockEditor';
